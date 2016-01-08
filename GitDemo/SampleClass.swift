4720e6c0e18f38edaa6177d9224367bfc3153b83
rtrevivian <richardtrevivian@hotmail.com>
2016-01-08 10:08:18 -0600

    SampleClass added

---
+++ GitDemo/SampleClass.swift
@@ -0,0 +1,13 @@
+//
+//  SampleClass.swift
+//  GitDemo
+//
+//  Created by Richard Trevivian on 1/8/16.
+//  Copyright © 2016 Richard Trevivian. All rights reserved.
+//
+
+import Foundation
+
+class SampleClass {
+    var str = "Hello world"
+}
